ea561301896045d0d91f7c8818c43dceb2257060
Rodion <kim.Radik@mail.ru>
2021-04-16 22:41:02 +0600

    Update Mapper

--- WeatherAPI/MetricsAgent/MapperProfile.cs
+++ WeatherAPI/MetricsAgent/MapperProfile.cs
@@ -10,19 +10,18 @@ namespace MetricsAgent
 {
     public class MapperProfile : Profile
     {
-        private DateTimeOffset _UNIX = new DateTime(1970, 01, 01);
         public MapperProfile()
         {
             CreateMap<CpuMetric, CpuMetricDto>()
-                .ForMember("Time", opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
+                .ForMember(dto => dto.Time, opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
             CreateMap<DotNetMetric, DotNetMetricDto>()
-                .ForMember("Time", opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
+                .ForMember(dto => dto.Time, opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
             CreateMap<NetworkMetric, NetworkMetricDto>()
-                .ForMember("Time", opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
+                .ForMember(dto => dto.Time, opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
             CreateMap<HddMetric, HddMetricDto>()
-                .ForMember("Time", opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
+                .ForMember(dto => dto.Time, opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
             CreateMap<RamMetric, RamMetricDto>()
-                .ForMember("Time", opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
+                .ForMember(dto => dto.Time, opt => opt.MapFrom(c => DateTimeOffset.FromUnixTimeSeconds((long)c.Time.TotalSeconds)));
         }
     }
 }
